d50c35b656ec537adfeb64e27cf1e98cadbd4717
Simon Rodil <simon@codecrew.us>
2026-06-06 21:19:48 +0200

    Correcting index and other files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/simon-rodil-cv/',
   plugins: [react(), tailwindcss()],
 })
